2dfb7d5722d79f1a3f17398b96c55a6f87a1f33f
Lucy Wang <swang1@babson.edu>
2022-12-04 18:08:41 -0500

    updated

--- app_customer.py
+++ app_customer.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for 
+from flask_sqlalchemy import SQLAlchemy
 import waitlist_recorder
 from waitlist_recorder import Customer
 import sqlite3
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URL'] = 'customer.db'
+# db = SQLAlchemy(app)
+
+# class Customer(db.Model): 
+#     name = db.Column(db.String)
+#     conatct = db._Column(db.String)
+
+connection = sqlite3.connect("waitlist.db") 
+connection.commit()
+cursor = connection.cursor()
 
 @app.route("/")
 def home(): 
@@ -19,14 +30,8 @@ def get_waiting_info():
         name = request.form['name']
         contact = request.form['contact']
         group_size = request.form['group_size']
-        cust = Customer(name, contact, group_size) 
-        with sqlite3.connect("wailist.db") as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO customer_info VALUES (?, ?, ?);", (cust.name, cust.contact, cust.group))
-            
-            con.commit()
-
-        # waitlist_recorder.insert_customer_info(cust)
+        cust= Customer(name, contact, group_size)
+        waitlist_recorder.insert_customer_info(cust)
         num_groups_waiting, waiting_time = waitlist_recorder.count_numbers_waiting_and_time(cust)
         table, table_size = waitlist_recorder.define_table_size(cust)
         return render_template("waiting_info.html",num_groups = num_groups_waiting, time = waiting_time, size = table_size)
@@ -55,3 +60,4 @@ def call_small():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=50000)
+    connection.commit()
